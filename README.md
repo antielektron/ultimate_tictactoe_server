d0c8009834b44a5d3aced208180ba885fcb9e7b3
Jonas Weinz <s6jowein@uni-bonn.de>
2019-03-10 00:29:52 +0100

    first server version

--- README.md
+++ README.md
@@ -1,3 +1,91 @@
 # ultimate_tictactoe_server
 
-a python server backend for ultimate tic-tac-toe
+a python server backend for ultimate tic-tac-toe.
+
+communication with the web client is done by a (far from any standard and almost random) json protocol:
+
+
+
+**register as player and in game queue:**
+
+```json
+{
+    "type": "register",
+    "data": {
+        "id": "<player_id>",
+   		"name": "<player_name>"
+    }
+}
+```
+
+response:
+
+```JSON
+{
+    "type": "register_response",
+    "data": {
+        "success": true,
+        "msg": "<additional info e.g. in case of error>"
+    }
+}
+```
+
+
+
+**message from server that game started**
+
+```json
+{
+    "type": "game_starts",
+    "data": {
+        "msg": "...",
+        "opponent_name": "...",
+        "is_first_move": true
+    }
+}
+```
+
+note: `is_first_move` indicates whether the player or it's opponent begins
+
+
+
+**move**
+
+```json
+{
+    "type": "move",
+    "data": {
+        "sub_x": "...",
+        "sub_y": "...",
+        "x": "...",
+        "y": "..."
+    }
+}
+```
+
+response:
+
+```json
+{
+    "type": "move_response",
+    "data": {
+        "success": true,
+        "msg": "..."
+    }
+}
+```
+
+
+
+**end game**
+
+```json
+{
+    "type": "end_game",
+    "data": {
+        "msg": "..."
+    }
+}
+```
+
+(response?)
---
+++ main.py
@@ -0,0 +1,65 @@
+import asyncio
+import websockets
+
+from game_manager import *
+
+
+async def socket_worker(websocket, path):
+
+    registered = False
+    id = None
+
+    print("new connection")
+
+    try:
+        # get first message as register message
+        raw_msg = await websocket.recv()
+
+        msg = json.loads(raw_msg)
+
+        if msg['type'] != 'register':
+            print("got wrong registration")
+            websocket.close()
+            return
+
+        id = msg['data']['id']
+
+        registered = await register_user(id, websocket)
+
+        register_response = {
+            'type': 'register_response', 'data': {
+                'success': True, 'msg': '...'}}
+
+        if not registered:
+            register_response['data']['success'] = False
+
+            await websocket.send(json.dumps(register_response))
+            websocket.close()
+            return
+
+        await websocket.send(json.dumps(register_response))
+
+        print("successful redisterd user " + id)
+
+        async for m in websocket:
+            await process_message(id, m)
+
+    except Exception as e:
+        # TODO: each disconnect is an exception so far
+        if id is not None:
+            print("catched exception in worker for user: " + id + ": " + str(e))
+        else:
+            print("catched exception in worker for unknown user")
+
+    finally:
+        if registered:
+            await unregister_user(id)
+
+        if id is None:
+            id = "unknown_user"
+        print("close connection to user: " + id)
+
+start_server = websockets.serve(socket_worker, 'localhost', 5555)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
